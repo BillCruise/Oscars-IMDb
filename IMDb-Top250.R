7f465196f93a1fbd87c7fbc8c9a2c13bc2991081
Bill Cruise <cruise02@gmail.com>
2016-02-13 12:52:52 -0500

    Added IMDb rank columns

--- IMDb-Top250.R
+++ IMDb-Top250.R
@@ -1,18 +1,25 @@
-# Get the Top 250 list from IMDb
+# Get the Top 250 Films list from IMDb
 
 library(rvest)
 library(stringr)
 
 html <- read_html("http://www.imdb.com/chart/top")
 
+# IMDb ranks are in a <td> element with the class "titleColumn"
+ranks <- html %>%
+    html_nodes("td.titleColumn") %>%
+    html_text(trim=TRUE) %>%
+    str_replace("\\.(.*)", "") %>% # remove everything after the "." (inclusive)
+    as.numeric()
+
 # Film titles are in a <td> element with the class "titleColumn" inside an <a> element
 titles <- html %>%
-    html_nodes(".titleColumn a") %>%
+    html_nodes("td.titleColumn a") %>%
     html_text()
 
 # Year of film release is in a <span> with the class "secondaryInfo"
 years <- html %>%
-    html_nodes(".secondaryInfo") %>%
+    html_nodes("span.secondaryInfo") %>%
     html_text() %>%
     str_replace("\\(", "") %>% # remove open paren
     str_replace("\\)", "") %>% # remove close paren
@@ -20,8 +27,8 @@ years <- html %>%
 
 # Ratings are in a <td> element with the class "imdbRating" inside a <strong> element
 ratings <- html %>%
-    html_nodes(".imdbRating strong") %>%
+    html_nodes("td.imdbRating strong") %>%
     html_text() %>%
     as.numeric()
 
-imdb.top.250 <- data.frame(Title = titles, Year = years, Rating = ratings)
+imdb.top.250 <- data.frame(Rank=ranks, Title=titles, Year=years, Rating=ratings)
